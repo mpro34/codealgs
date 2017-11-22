56f79483b3dbcda113562469a6c676fc6d4d496a
Chris Whiting <mpro34@gmail.com>
2017-11-21 20:23:31 -0800

    Added 1.3 question and fixed bug

--- array_strings.cpp
+++ array_strings.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 /*
- * Check is input string s is a unique string
+ * 1.1 Check is input string s is a unique string
  */
-bool isUnique( string s ) {
+bool isUnique (string s) {
   map<char,int> charmap;
   for (int i=0; i < s.size(); ++i) {
     if ( charmap[s.at(i)] == 1 )
@@ -18,9 +18,9 @@ bool isUnique( string s ) {
 }
 
 /*
- * Check if string a is a permutation of string b
+ * 1.2 Check if string a is a permutation of string b
  */
-bool checkPerm( string a, string b ) {
+bool checkPerm (string a, string b) {
   int aCount=0;
   int bCount=0;
   if (a.size() != b.size()) {
@@ -36,13 +36,33 @@ bool checkPerm( string a, string b ) {
   return true;
 }
 
+/*
+ * 1.3 Replace any spaces in input string with "%20"
+ */
+string URLify (string s, int size) {
+  for (int i=0; i<size; ++i) {
+    if (s[i] == ' ') {
+      s.insert (i, "%20");
+      s.erase(s.begin()+i+3);
+    }
+  }
+  return s;
+}
+
+
 
 int main() {
+//1.1
   string mystring = "chgis";
   cout << isUnique(mystring) << endl;
-  
+//1.2  
   string a = "racecar";
   string b = "acecar";
   cout << checkPerm(a, b) << endl;
+//1.3
+  string s = "Chris whiting is me         ";
+  int s_size = s.size();
+  cout << URLify(s, s_size) << endl;
+
   return 0;
 }
