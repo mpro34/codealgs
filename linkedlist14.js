be8c84ec82cf734ef83eb2fa5b61ed13949a9e3a
mpro34 <mpro34@gmail.com>
2018-03-15 16:39:39 -0700

    Added outline for 14 ll questions

---
+++ linkedlist14.js
@@ -0,0 +1,168 @@
+//CodeRust Interview Questions - Linked Lists
+
+/* 1.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 2.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 3.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 4.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 5.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 6.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 7.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 8.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 9.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 10.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 11.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 12.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 13.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
+
+
+/* 14.
+  * A.
+  * B.
+*/
+let  = function() {
+
+}
+//  Time - O()
+//  Space - O()
+/* TODO NOTES -  */
